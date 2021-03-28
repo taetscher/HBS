96b0d9e5a65e4b3618fad2d18a5bb792ea2fdad9
Benjamin Schüpbach <auteblauwau@hotmail.com>
2021-03-28 15:23:42 +0200

    closes #7, xpath library in options.py

--- options.py
+++ options.py
@@ -2,7 +2,7 @@
 
 # --------------------GENERAL OPTIONS-------------------------------------
 # Game Progressions
-scrapeGameProgressions = True
+scrapeGameProgressions = False
 plotGameProgressions = False
 
 # Player Progress
@@ -14,21 +14,18 @@ scrapePlayerStats = False
 plotPlayerStats = False
 # -----------------------------------------------------------------------------
 
+
 # --------------------TEAM/SEASONS SELECTION-------------------------------------
 # SHV-Numbers needed to get the right data. Adjust teams and seasons being processed here:
 
-teams_seasons = {'Wacker Thun U15 Elite':{'Saison 20_21':[32054]},
-                 'Wacker Thun U17 Elite':{'Saison 20_21':[31846]},
-                 'Wacker Thun U19 Elite':{'Saison 20_21':[31823]},
-                 'TV Steffisburg NLB':{'Saison 20_21':[32296]},
-                 'Wacker Thun NLA':{'Saison 20_21':[31821]}
+teams_seasons = {"Wacker Thun U15 Elite": {"Saison 20_21":[32054]},
+                 "Wacker Thun U17 Elite": {"Saison 20_21":[31846]},
+                 "Wacker Thun U19 Elite": {"Saison 20_21":[31823]},
+                 "TV Steffisburg NLB": {"Saison 20_21":[32296]},
+                 "Wacker Thun NLA": {"Saison 20_21":[31821]}
                  }
-# -----------------------------------------------------------------------------
-
-
 
 # old configurations
-
 # teams_seasons = {'Wacker Thun U15 Elite':{'Saison 20_21':[32054],'Saison 19_20':[30639],'Saison 18_19':[28999]},
 #                 'Wacker Thun U17 Elite':{'Saison 20_21':[31846],'Saison 19_20':[30635],'Saison 18_19':[29811]},
 #                 'Wacker Thun U19 Elite':{'Saison 20_21':[31823],'Saison 19_20':[30371], 'Saison 18_19':[28998]},
@@ -39,3 +36,29 @@ teams_seasons = {'Wacker Thun U15 Elite':{'Saison 20_21':[32054]},
 
 # teams_seasons = {'TV Steffisburg 3': {'Saison 19_20':[31089],'Saison 18_19':[29306]}
 #                 }
+# -----------------------------------------------------------------------------
+
+
+# --------------------XPATHS FOR SCRAPERS-------------------------------------
+# xpaths to information relevant to the scrapers
+
+xpaths = {"playerProgress":{"team_name": r"/html/body/div[2]/div[3]/div/div/div[2]/div/div[2]/h1",
+                            "games_button": r'//*[@id="games-tab"]',
+                            "first_date": r'//*[@id="dateFromGames_1"]',
+                            "second_date": r'//*[@id="dateToGames_1"]',
+                            "click_away": r'//*[@id="games"]/div/div[1]/h2',
+                            "stats_tab": r'//*[@id="stats-tab"]',
+                            "game_date": r'/html/body/div[2]/div[1]/div[1]/div/div/div[2]/div[2]/div[3]/span[1]',
+                            "left_table": r'//*[@id="stats"]/div[2]/div[3]/div[1]/div/table',
+                            "right_table": r'//*[@id="stats"]/div[2]/div[3]/div[2]',
+                            "left_team": r'//*[@id="stats"]/div[2]/div[3]/div[1]/div/table/thead[1]/tr/td/span',
+                            "right_team": r'//*[@id="stats"]/div[2]/div[3]/div[2]/div/table/thead[1]/tr/td/span',
+                            "league": r'/html/body/div[2]/div[1]/div[2]/div/div/div[6]/p',
+                            "date_info": "Cannot be externalized, in getAllGames() inside scraperPlayerProgress.py"
+                            },
+          "gameProgressions":{"tab": r'//*[@id="live-tab"]',
+                              "table": r'//*[@id="live"]/div[2]/div[3]',
+                              "date": r'/html/body/div[2]/div[1]/div[1]/div/div/div[2]/div[2]/div[3]/span[1]'
+                              }
+          }
+# -----------------------------------------------------------------------------
